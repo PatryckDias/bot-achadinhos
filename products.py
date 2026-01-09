f33c2c80c0c7b5624dd2b84ba82aa60b0b9dcad2
PatryckDias <patryckdias4@gmail.com>
2026-01-09 17:06:16 -0300

    Create products.py

---
+++ products.py
@@ -0,0 +1,14 @@
+PRODUCTS = [
+    {
+        "site": "amazon",
+        "url": "https://www.amazon.com.br/dp/SEU_PRODUTO"
+    },
+    {
+        "site": "shopee",
+        "url": "https://shopee.com.br/SEU_PRODUTO"
+    },
+    {
+        "site": "mercadolivre",
+        "url": "https://produto.mercadolivre.com.br/SEU_PRODUTO"
+    }
+]
