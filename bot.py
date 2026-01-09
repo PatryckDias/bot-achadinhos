51ee4416f71599f6b1b26026fd605d6517c57d7a
PatryckDias <patryckdias4@gmail.com>
2026-01-09 18:34:22 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,21 @@
 import os
 from telegram import Bot
 from products import PRODUCTS
-from scrapers.shopee import get_shopee_price
+from scrapers.mercadolivre import get_ml_price
 
 def main():
     bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
     for product in PRODUCTS:
-        if product["site"] == "shopee":
-            data = get_shopee_price(product["url"])
+        if product["site"] == "mercadolivre":
+            data = get_ml_price(product["url"])
 
             if not data:
                 continue
 
             msg = (
-                f"🔥 *Oferta Shopee*\n\n"
+                f"🔥 *Oferta Mercado Livre*\n\n"
                 f"{data['title']}\n\n"
                 f"💰 Preço: R$ {data['price']:.2f}\n"
                 f"🔗 {data['url']}"
