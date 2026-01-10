d81eb750d6a43cd7c2078e93280f73e86f31e681
PatryckDias <patryckdias4@gmail.com>
2026-01-10 13:27:11 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,18 @@
-""
+import sqlite3
+
+conn = sqlite3.connect("sent.db")
+c = conn.cursor()
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS sent (
+    id TEXT PRIMARY KEY
+)
+""")
+
+def already_sent(pid):
+    c.execute("SELECT 1 FROM sent WHERE id=?", (pid,))
+    return c.fetchone() is not None
+
+def mark_sent(pid):
+    c.execute("INSERT OR IGNORE INTO sent VALUES (?)", (pid,))
+    conn.commit()
