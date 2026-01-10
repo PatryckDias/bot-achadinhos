e0a96820f80c3f2c7c4ea707ec6c9c8530120a84
PatryckDias <patryckdias4@gmail.com>
2026-01-10 17:10:44 -0300

    Update categories.py

--- categories.py
+++ categories.py
@@ -2,11 +2,11 @@ CATEGORIES = [
     {
         "name": "Acessórios Gamer",
         "query": "acessórios gamer ps5",
-        "max_price": 300
+        "max_price": 3000
     },
     {
         "name": "Jogos PS5",
         "query": "jogo ps5 mídia física",
-        "max_price": 250
+        "max_price": 2500
     }
 ]
