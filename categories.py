9c5d2a1c9dd0c6b672f3408642558f459c16211d
PatryckDias <patryckdias4@gmail.com>
2026-01-10 09:53:18 -0300

    Create categories.py

---
+++ categories.py
@@ -0,0 +1,13 @@
+CATEGORIES = [
+    {
+        "name": "Games",
+        "url": "https://www.mercadolivre.com.br/consoles-e-video-games",
+        "max_price": 500
+    },
+    {
+        "name": "Acessórios Gamer",
+        "url": "https://www.mercadolivre.com.br/acessorios-para-games",
+        "max_price": 300
+    }
+]
+
