8bac98304981ab434c746c283e9a907207531f19
PatryckDias <patryckdias4@gmail.com>
2026-01-10 11:00:50 -0300

    Update categories.py

--- categories.py
+++ categories.py
@@ -2,11 +2,11 @@ CATEGORIES = [
     {
         "name": "Acessórios Gamer",
         "query": "acessorios gamer",
-        "min_discount": 25
+        "min_discount": 15
     },
     {
         "name": "Jogos PS5",
         "query": "jogos ps5",
-        "min_discount": 25
+        "min_discount": 15
     }
 ]
