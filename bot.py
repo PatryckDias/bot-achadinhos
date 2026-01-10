f1600b4de1a921d28a693cbe2e6a1234b260e602
PatryckDias <patryckdias4@gmail.com>
2026-01-10 08:44:44 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,31 @@
 import os
-from telegram import Bot
+import requests
 from products import PRODUCTS
 from scrapers.mercadolivre import get_ml_price
 
-def main():
-    bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
+
+def send_telegram_message(text):
+    token = os.getenv("TELEGRAM_TOKEN")
     chat_id = os.getenv("TELEGRAM_CHAT_ID")
 
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+
+    r = requests.post(url, json=payload)
+    print("Telegram status:", r.status_code, r.text)
+
+
+def main():
     for product in PRODUCTS:
         if product["site"] == "mercadolivre":
             data = get_ml_price(product["url"])
+
+            print("DEBUG DATA:", data)
 
             if not data:
                 continue
@@ -21,11 +37,8 @@ def main():
                 f"🔗 {data['url']}"
             )
 
-            bot.send_message(
-                chat_id=chat_id,
-                text=msg,
-                parse_mode="Markdown"
-            )
+            send_telegram_message(msg)
+
 
 if __name__ == "__main__":
     main()
