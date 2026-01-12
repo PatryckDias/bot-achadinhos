63da4970574d42892ad98e76e2378a818615356d
PatryckDias <patryckdias4@gmail.com>
2026-01-12 09:28:11 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,7 @@
-import os
-from telegram import Bot
-from scrapers.mercadolivre_api import get_ml_deals
+print("🔥 BOT FUNCIONANDO 🔥")
 
-print("🔥 BOT EXECUTANDO 🔥")
+import time
 
-BOT = Bot(os.getenv("TELEGRAM_TOKEN"))
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-getenv("TELEGRAM_CHAT_ID")
-
-def run_bot():
-    deals = get_ml_deals("ps5")
-
-    print("[BOT] Ofertas encontradas:", len(deals))
-
-def main():
-    print("Buscando ofertas...")
-    deals = get_ml_deals("ps5")
-    print("Ofertas recebidas:", deals)
-
-    for d in deals:
-        msg = (
-            f"🔥 *OFERTA MERCADO LIVRE*\n\n"
-            f"{d['title']}\n\n"
-            f"💰 De R$ {d['original']:.2f}\n"
-            f"💥 Por R$ {d['price']:.2f}\n"
-            f"📉 {d['discount']}% OFF\n\n"
-            f"🛒 {d['url']}"
-        )
-
-        BOT.send_message(CHAT_ID, msg, parse_mode="Markdown")
-        print("Mensagem enviada")
-
-if __name__ == "__main__":
-    print("🤖 Bot iniciado...")
-
-    while True:
-        try:
-            run_bot()
-        except Exception as e:
-            print("Erro:", e)
-
-        print("⏳ Aguardando 10 minutos...")
-        time.sleep(300)   # 10 minutos
+while True:
+    print("Estou vivo...")
+    time.sleep(5)
